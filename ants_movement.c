4768b376b3b8830664ff0ec227d8f079d4e3fee3
Fermin Galan repiso <fgalan-r@c3r10s1.42malaga.com>
2025-08-13 19:12:27 +0200

    assign ants

--- ants_movement.c
+++ ants_movement.c
@@ -34,14 +34,80 @@ size_t num_lines(unsigned short ants, size_t *paths_len, size_t n_paths)
     return (lines);
 }
 
+unsigned short *assign_ants(unsigned short ants, size_t *paths_len, size_t n_paths)
+{
+    unsigned short  *assigned = malloc(sizeof(unsigned short *) * n_paths);
+    if (n_paths == 1)
+    {
+        assigned[0] = ants;
+        return(assigned);
+    }
+    //ordenar paths len de menor a mayor
+    size_t i = 0;
+    size_t j = 0;
+    size_t dif_paths = 0;
+    size_t open_paths = 1;
+    unsigned short total_ants = ants - 1;
+    assigned[0] = 1;
+
+    while (i < n_paths - 1 && total_ants > 0)
+    {
+        dif_paths = paths_len[i + 1] - paths_len[i];
+        j = 0;
+        if (total_ants - ((dif_paths * i ) + paths_len[i]) < 0)
+            break ;
+        assigned[i] += paths_len[i];
+        total_ants -= paths_len[i];
+        while (j <= i)
+        {
+            total_ants -= dif_paths;
+            assigned[j] += dif_paths;
+            j++;
+        }
+        if (total_ants > (dif_paths * i))
+        {
+            assigned[i + 1] = 1;
+            total_ants -= 1;
+            open_paths++;
+        }
+        else
+            break ;
+        i++;
+    }
+    //repatir resto de hormigas
+    if (total_ants > 0)
+    {
+        unsigned short rest = total_ants / open_paths;
+        i = 0;
+        while (i < open_paths)
+        {
+            assigned[i] += rest;
+            i++;
+        }
+        if (total_ants % open_paths != 0)
+            assigned[0] += 1;
+    }
+
+
+    return (assigned);
+}
+
 int main(void)
 {
     unsigned short ants = 100;
-    size_t *lens = malloc(sizeof(size_t) * 3);
-    lens[0] = 20;
+    size_t n_paths = 3;
+    size_t *lens = malloc(sizeof(size_t) * n_paths);
+    lens[0] = 5;
     lens[1] = 20;
-    lens[2] = 30;
-    size_t lines = num_lines(ants, lens, 3);
+    lens[2] = 50;
+    size_t lines = num_lines(ants, lens, n_paths);
     printf("lines: %ld\n", lines);
+    unsigned short *paths = assign_ants(ants, lens, n_paths);
+    size_t i = 0;
+    while (i < n_paths)
+    {
+        printf("path[%ld] %d\n", i, paths[i]);
+        i++;
+    }
     return (0);
 }
